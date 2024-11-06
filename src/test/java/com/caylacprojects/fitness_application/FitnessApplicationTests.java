a0e37700af571409aed4d98409609f9ce2e8c558
Cayla <cay.curry01@gmail.com>
2024-11-06 15:13:24 -0500

    add basic springboot and react files

---
+++ src/test/java/com/caylacprojects/fitness_application/FitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caylacprojects.fitness_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
